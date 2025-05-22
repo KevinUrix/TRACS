9a6ea8b74bf3e516f19a27b9e7c37e64de5a9295
KevinUrix <kugaona.kg@gmail.com>
2025-05-22 15:06:12 -0600

    Nuevo componente para darle estilo al botón de imprimir, se agrego un logo de CUCEI como fondo del main-container (sigue en pruebas) modificaciones en selectsLogic para el botón y ajustes a calendar.css.

---
+++ src/components/interfaz_calendar/printButton.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { handlePrint } from './utils';
+
+// PrintButton.js
+export default function PrintButton({ selectedBuilding, selectedDay, selectedCycle, onPrint }) {
+  const handleClick = () => {
+    onPrint(selectedBuilding, selectedDay, selectedCycle);
+  };
+
+  return (
+    <button onClick={handleClick} className="bg-purple-500 rounded-full px-3 py-1 shadow-md text-white">
+      <b>Imprimir tabla</b>
+    </button>
+  );
+}
